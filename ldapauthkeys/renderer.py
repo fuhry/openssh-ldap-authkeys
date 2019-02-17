5b4b0e1e5a9b0b191c9638bcf11f6bb28cba6069
Dan Fuhry <dfuhry@datto.com>
2019-02-16 19:51:51 -0500

    Documentation and empty line trimming

--- ldapauthkeys/renderer.py
+++ ldapauthkeys/renderer.py
@@ -12,16 +12,16 @@ def render_authkeys(authkeys, config, key_type=None, key_value=None):
     Render a hash of authorized keys that came either from a YAML cache file or
     fetch_ldap_authkeys(). Returns a string.
     """
-    
+
     output = []
     ssh_key_attr = config['ldap']['attributes']['ssh_key']
-    
+
     for username in authkeys.keys():
         user_entry = authkeys[username]
-        
+
         for k in user_entry[ssh_key_attr]:
             kt, kv = k.split(' ')
             if (key_type is None or key_type == kt) and (key_value is None or key_value == kv):
                 output.append(print_authkey(kt, kv, username, config))
-    
+
     return '\n'.join(output)
--- ldapauthkeys/resolver.py
+++ ldapauthkeys/resolver.py
@@ -10,13 +10,13 @@ class SrvRecord:
     port = None
     priority = 0
     weight = 0
-    
+
     def __init__(self, hostname, port, priority, weight):
         self.hostname = hostname.rstrip('.')
         self.port     = port
         self.priority = priority
         self.weight   = weight
-    
+
 def sort_srv_results(results):
     """
     Sort a list of SrvRecords by weight and priority.
@@ -30,10 +30,10 @@ def protocolize(records, protocol):
     "protocol://{record.host}:{record.port}".
     """
     result = []
-    
+
     for r in records:
         result.append("%s://%s:%d" % (protocol, r.hostname, r.port))
-    
+
     return result
 
 def basedn_to_domain(basedn):
@@ -42,16 +42,16 @@ def basedn_to_domain(basedn):
     """
     if not isinstance(basedn, str):
         raise TypeError("basedn is expected to be a string")
-        
+
     basedn = basedn.lower()
     expr = re.compile(r'^(dc=[a-z0-9-]+)(, *dc=[a-z0-9-]+)*$')
-    
+
     if not expr.match(basedn):
         raise ValueError("basedn must consist of only \"dc\" components to use DNS SRV lookup")
-    
+
     domain = []
     basedn = re.split(', *dc=', basedn[3:])
-    
+
     return '.'.join(basedn)
 
 def lookup_srv(record):
@@ -59,11 +59,11 @@ def lookup_srv(record):
     Perform a SRV record lookup, returning a list of SrvRecord objects.
     """
     answer = dns.resolver.query(record, 'SRV')
-    
+
     result = []
     for rr in answer.rrset:
         result.append(SrvRecord(rr.target.to_unicode(), rr.port, rr.priority, rr.weight))
-    
+
     return sort_srv_results(result)
 
 def resolve_ldap_srv(basedn):
@@ -73,15 +73,15 @@ def resolve_ldap_srv(basedn):
     "ldap(s)://server:port".
     """
     domain = basedn_to_domain(basedn)
-        
+
     # try LDAPS first
     results = lookup_srv('_ldaps._tcp.%s' % (domain))
     if len(results):
         return protocolize(results, 'ldaps')
-    
+
     # then try LDAP
     results = lookup_srv('_ldap._tcp.%s' % (domain))
     if len(results):
         return protocolize(results, 'ldap')
-    
+
     raise RuntimeError("Couldn't find any LDAP SRV records for %s" % (domain))
